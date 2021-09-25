1b7a8e88dae73f8d3257b1fea328a5065b67d3c6
ksn38 <ksn38@yandex.ru>
2021-09-25 17:54:59 +0300

    add fl.py

---
+++ fl.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+from os import listdir
+from os.path import isfile, join
+import re
+
+
+mypath = 'fl/keywords'
+files = [i for i in listdir(mypath)]
+
+for k in files:
+    df = pd.read_csv(mypath + '/' + k)
+    df.columns = ['tag', 'val']
+    df['norm_val'] = df.val/df.val.iloc[0]
+    df['Date'] = re.findall('[0-9]{4}-[0-9]{2}-[0-9]{2}', k)[0]
+    df[['Date', 'tag', 'norm_val']].iloc[:149].to_csv('fl/keywords_norm' + '/' + k, header=False, index=False, encoding='utf-8', sep=',')
